55347173706451de4e9c784ed301bc3a8429b1a4
elkhadragydxc <mahmoud.elkhadragy@dxc.com>
2023-12-27 16:36:14 +0200

    review code

--- index.js
+++ index.js
@@ -26,161 +26,37 @@ function addChanges() {
   document.querySelector("head").insertAdjacentHTML(
     "beforeend",
     `
-        <style>
-         .new-element-container {
-            position: relative;
-            background-color: #a1c6ce;
-            padding:0px 10px 10px;
-          }
-         .second-div{
-          display: flex;
-        flex-direction: column;
-        align-items: center;
-            clip-path: polygon(0 34%, 100% 19%, 100% 100%, 0% 100%);
-            background-color: #FFFFFF;
-            padding:160px 40px 60px;
-          }
-         .new-element-img{
-            position: absolute;
-            right: 0px;
-            top:0px;
-            z-index: 9;
-          }
-         .second-div p{
-            font-family: 'Libre Baskerville',sans-serif;
-            font-size: 32px;
-            font-weight: 400;
-            line-height: 38px;
-            letter-spacing: 0em;
-            text-align: center;
-            color:#003D68;
-            padding:40px 0px 0px;
-          }
-         .second-div input{
-            width: 100%;
-            background-color: #003D68;
-            font-family: 'Roboto';
-            font-size: 21px;
-            font-weight: 400;
-            line-height: 32px;
-            letter-spacing: 0em;
-            text-align: center;
-            color:#FFFFFF;
-            margin-top: 10px;
-          }
-          @media only screen and (max-width:  906px){
-            .second-div p{
-              font-size: 27px;
-              padding:20px 0px 0px;
-            }
-            .second-div{
-              padding:160px 20px 40px;
-            }
-          }
-          @media only screen and (max-width: 749px){
-            .small--one-half:nth-child(3){
-              width:100%;
-            }
-            .second-div p{
-              font-size: 42px;
-              letter-spacing: 1px;
-              padding:40px 0px 0px;
-            }
-            .second-div{
-              padding:160px 40px 40px;
-            }
-          }
-          @media only screen and (max-width:  385px){
-            .second-div p{
-              font-size: 32px;
-              letter-spacing: 0rem;
-            }
-            .second-div{
-              padding:160px 20px 40px;
-            }
-          }
-          @media only screen and (max-width:257px){
-            .second-div p{
-              font-size: 23px;
-            }
-            .second-div input{
-              font-size: 13px;
-            }
-            .second-div{
-              padding:160px 10px 40px;
-            }
-          }   
-  </style>
+    <style>
+    @import url(https://fonts.googleapis.com/css2?family=Libre+Baskerville&display=swap);.second-div a,.second-div p{font-weight:400;letter-spacing:0;text-align:center}.new-element-container{position:relative;background-color:#a1c6ce;padding:0 10px 10px}.second-div{display:flex;flex-direction:column;align-items:center;clip-path:polygon(0 34%,100% 19%,100% 100%,0% 100%);background-color:#fff;padding:160px 40px 50px}.new-element-img{position:absolute;right:0;top:0;z-index:9;max-width:250px}.second-div p{font-family:"Libre Baskerville",serif;font-size:32px;line-height:38px;color:#003d68;padding:40px 0 20px;margin:0}.second-div p br{display:none}.second-div a{width:100%;background-color:#003d68;font-family:Roboto;font-size:21px;line-height:32px;color:#fff;padding:10px;transition:.2s ease-in-out}.second-div a:hover{opacity:.85}@media (max-width:1000px){.second-div p{font-size:21px;line-height:27px}.second-div{padding:160px 10px 40px}.new-element-img{max-width:215px}}@media (max-width:749px){#Collection .grid.grid--uniform.grid--view-items{display:flex;flex-wrap:wrap}#Collection .grid.grid--uniform.grid--view-items>div.grid__item{order:4}#Collection .grid.grid--uniform.grid--view-items>div.grid__item:first-child{order:1}#Collection .grid.grid--uniform.grid--view-items>div.grid__item:nth-child(2){order:3}#Collection .grid.grid--uniform.grid--view-items>div.grid__item:nth-child(3){order:2}#Collection .grid.grid--uniform.grid--view-items>div.grid__item.ca_lt_011_signup{width:100%}.second-div p{font-size:22px;line-height:30px}.second-div p br{display:block}.second-div{padding:160px 20px 30px}.new-element-img{max-width:230px}}@media (max-width:400px){.second-div p{font-size:19px;line-height:24px;letter-spacing:0}.second-div{padding:143px 10px 40px}}@media (max-width:335px){.second-div p{font-size:17px;line-height:22px}}
+    </style>
     `
   );
   //========================< website new element >========================================
   const gridContainer = document.querySelector(".grid--view-items");
-  const bodyy = document.querySelector("body");
   const secondElement = gridContainer.querySelector(
     ".medium-up--one-third:nth-child(2)"
   );
   const secondElementClass = secondElement ? secondElement.className : "";
   if (gridContainer) {
     const newElement = document.createElement("div");
-    if (newElement) {
-      newElement.innerHTML = `
-    <div class="new-element-container">
-      <img class="new-element-img"
-        src="https://conversionadvocates.com/exp-images/lt/lt-011/ca_back.png"
-        alt=""
-      /> 
-      <div class="second-div">
-        <p>Learn about our latest offers and promotions!</p>
-        <input type="button" value="Sign Me Up" onclick="window.location.href='https://legacytouch.com/pages/email-signup';" />
-      </div>   
-    </div>
-  `;
-      newElement.className = secondElementClass;
-    }
-    const newMobileElement = document.createElement("div");
-    if (newMobileElement) {
-      newMobileElement.innerHTML = `
-    <div class="new-element-container">
-      <img class="new-element-img"
-        src="https://conversionadvocates.com/exp-images/lt/lt-011/ca_back.png"
-        alt=""
-      /> 
-      <div class="second-div">
-        <p>Learn about our latest offers and promotions!</p>
-        <input type="button" value="Sign Me Up" onclick="window.location.href='https://legacytouch.com/pages/email-signup';" />
-      </div>   
-    </div>
-  `;
-      newMobileElement.className = secondElementClass;
-    }
-    const checkAndInsertElements = () => {
-      if (window.innerWidth <= 749) {
-        bodyy.classList.add("mobile");
-        // console.log(bodyy.classList);
-        if (bodyy.classList.contains("mobile")) {
-          newElement.remove();
-          gridContainer.insertBefore(
-            newMobileElement,
-            secondElement.nextSibling
-          );
-          // console.log("hjashkajkalalkls;al;;assl';a");
-        }
-      } else {
-        bodyy.classList.remove("mobile");
-        // console.log(bodyy.classList);
-        if (!bodyy.classList.contains("mobile")) {
-          newMobileElement.remove();
-          gridContainer.insertBefore(newElement, secondElement);
-        }
-      }
-    };
-    checkAndInsertElements();
 
-    window.addEventListener("resize", () => {
-      checkAndInsertElements();
-    });
-  } else {
-    console.log("Grid container not found.");
+    newElement.innerHTML = `
+      <div class="new-element-container">
+        <img class="new-element-img"
+          src="https://conversionadvocates.com/exp-images/lt/lt-011/ca_back.png"
+          alt=""
+        /> 
+        <div class="second-div">
+          <p>Learn about our latest <br>offers and promotions!</p>
+          <a href="https://legacytouch.com/pages/email-signup">Sign Me Up</a>
+        </div>
+      </div>
+    `;
+
+    newElement.className = secondElementClass;
+    newElement.classList.add("ca_lt_011_signup");
+
+    gridContainer.insertBefore(newElement, secondElement);
   }
 }
 //========================================================================================
